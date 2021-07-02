a8aad4ffee6d7f539b17a17d04ec5a2ba973d1d5
felix <you@example.com>
2021-07-02 13:53:38 +0300

    Initial Commit

---
+++ Python-Basics/Variables-demo.py
@@ -0,0 +1,25 @@
+#Declaring and assigning the value of the variable
+website="apple.com"
+print(website)
+
+#assigning a new value to the website
+website="programmiz.com"
+
+#print the new value to the screen
+print(website)
+
+#assigning the multiple values to the multiple variables
+a,b,c=5,3.6,"Hello"
+print(a)
+print(b)
+print(c)
+
+#Python DataType
+a = 5
+print(a, "is of type", type(a))
+
+a = 2.0
+print(a, "is of type", type(a))
+
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j,complex))
